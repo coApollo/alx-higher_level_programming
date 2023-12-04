972261c1122cb792d0839a119d7b417d4c21aee4
coApollo <bristarcollins@gmail.com>
2023-12-05 00:28:27 +0300

    pycodestyle

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -7,7 +7,6 @@ def new_in_list(my_list, idx, element):
     for i in range(length):
         new_list.append(my_list[i])
 
-
     if (idx < 0 or idx > length):
         return (new_list)
 
--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,11 +4,11 @@ def print_matrix_integer(matrix=[[]]):
     if (not matrix or not any(matrix)):
         return
     outer = len(matrix)
-    for i in range(outer):
+    for (i in range(outer)):
         inner = len(matrix[i])
-        for j in range(inner):
+        for (j in range(inner)):
             print("{:d}".format(matrix[i][j]), end='')
             if (j < inner - 1):
                 print(' ', end='')
         print()
-    return (None)
+
